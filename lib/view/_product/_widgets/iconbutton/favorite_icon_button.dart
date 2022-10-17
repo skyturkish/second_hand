3199632abdcd97730966474ed308ced5387bf9b0
sky <gokturk.acr2002@gmail.com>
2022-10-17 17:58:58 +0300

    from statefull --> from stateless

--- lib/view/_product/_widgets/iconbutton/favorite_icon_button.dart
+++ lib/view/_product/_widgets/iconbutton/favorite_icon_button.dart
@@ -4,30 +4,19 @@ import 'package:second_hand/core/extensions/context_extension.dart';
 import 'package:second_hand/core/init/notifier/user_information_notifier.dart';
 import 'package:second_hand/models/product.dart';
 
-@immutable
-class FavoriteIconButton extends StatefulWidget {
-  const FavoriteIconButton({
-    super.key,
-    required this.product,
-  });
-
+class FavoriteIconButton extends StatelessWidget {
+  const FavoriteIconButton({super.key, required this.product});
   final Product product;
 
   @override
-  State<FavoriteIconButton> createState() => _FavoriteIconButtonState();
-}
-
-class _FavoriteIconButtonState extends State<FavoriteIconButton> {
-  @override
   Widget build(BuildContext context) {
     final isFavorite =
-        context.watch<UserInformationNotifier>().userInformation.favoriteProducts.contains(widget.product.productId);
+        context.watch<UserInformationNotifier>().userInformation.favoriteProducts.contains(product.productId);
     return IconButton(
       onPressed: () {
         isFavorite
-            ? context.read<UserInformationNotifier>().removeFavoriteProduct(productId: widget.product.productId)
-            : context.read<UserInformationNotifier>().addFavoriteProduct(productId: widget.product.productId);
-        setState(() {});
+            ? context.read<UserInformationNotifier>().removeFavoriteProduct(productId: product.productId)
+            : context.read<UserInformationNotifier>().addFavoriteProduct(productId: product.productId);
       },
       icon: Icon(
         isFavorite ? Icons.favorite : Icons.favorite_border,
