384b6f90833c41c0c05f81f755699ebdc16bf9a7
sky <gokturk.acr2002@gmail.com>
2022-10-30 23:40:22 +0300

    open keyboard bug fix

--- lib/view/app/addproduct/include_some_details/view/include_some_details_view.dart
+++ lib/view/app/addproduct/include_some_details/view/include_some_details_view.dart
@@ -161,6 +161,8 @@ class NextButton extends StatelessWidget {
   Widget build(BuildContext context) {
     return CustomElevatedButton(
       onPressed: () {
+        // TODO bunu son kez checkle
+        FocusManager.instance.primaryFocus?.unfocus();
         if (formKey.currentState!.validate()) {
           context.read<SaleProductNotifier>().updateProduct(
                 title: titleController.text,
